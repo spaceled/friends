ded6215984ed4675a15fc39e76ac51f2e944269c
Rezaul Karim <spaceled@gmail.com>
2021-10-29 23:21:36 +0600

    ..........

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
 
 const Friend = (props) => {
     const {name, email, id} = props.friend;
+    const history = useHistory();
+
+    // This function can be use instead of <Link>
+    const handleClick = (friendId) => {
+        const url = `/about/${friendId}`;
+        history.push(url);
+    }
     const friendStyle = {
         border: '1px solid purple',
         margin: '20px',
@@ -15,7 +23,10 @@ const Friend = (props) => {
     <div style={friendStyle}>
             <h2>Name: {name}</h2>
             <p>Email: {email}</p>
-            <p><Link to={`/about/${id}`}>Show details of {id}</Link></p>
+            {/* <Link to={`/about/${id}`}>
+                <button>Show details of {id}</button>
+            </Link> */}
+            <button onClick={() => handleClick(id)}>Click Me</button>
         </div>
     );
 };
--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const FriendDetail = () => {
     const {friendId} = useParams();
+    const [friend, setFriends] = useState({});
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setFriends(data))
+
+    }, []);
     return (
         <div>
-            <h3>Detail Information {friendId}</h3>
+            <p>Detail Information {friendId}</p>
+            <h3>{friend.name}</h3>
+            <p>Email: {friend.email}</p>
+            <p>Phone: {friend.phone}</p>
+            <p>Website: {friend.website}</p>
         </div>
     );
 };
